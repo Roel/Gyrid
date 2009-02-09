05b1c6bc27e93396a7eac427c6275df5ae5db915
Roel Huybrechts <mail@rulus.be>
2009-02-09 21:55:10 +0100

    [gyrid @ mail@rulus.be-20090209205510-5s887o21yr2i0fab] added licence headers

--- daemon.py
+++ daemon.py
@@ -1,6 +1,12 @@
 #-*- coding: utf-8 -*-
 #
-# Source: http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+# This file belongs to Bluetracker.
+#
+# Bluetracker is a Bluetooth device scanner daemon.
+#
+# Source of this module:
+#  http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
 
 import sys, os, time, atexit
 from signal import SIGTERM 
