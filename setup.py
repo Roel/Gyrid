17bb6eb1f41f6317b71494dc01b8a9c53e43281c
Roel Huybrechts <mail@rulus.be>
2009-03-11 20:57:02 +0100

    [gyrid @ mail@rulus.be-20090311195702-1e2a02fddchudpzh] Added license, changelog, readme and setupscript.

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#
+# This file belongs to Bluetracker.
+#
+# Bluetracker is a Bluetooth device scanner daemon.
+# Copyright (C) 2009  Roel Huybrechts
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.dep_util import newer
+from distutils.log import info
+
+class InstallData(install_data):
+    """
+    Subclassing install_data to gzip ChangeLog and install it in the correct
+    location.
+    """
+    def run (self):
+        """
+        Call _gzip to zip the ChangeLog and install it to
+        /usr/share/doc/bluetracker.
+        """
+        self.data_files.extend (self._gzip ('ChangeLog', os.path.join('share',
+                                            'doc', 'bluetracker')))
+        install_data.run (self)
+
+    def _gzip(self, file, dest):
+        """
+        Zip the given file with gzip.
+
+        @param  file  The URL of the file to zip.
+        @param  dest  The URL of the folder to install the zipped file into.
+        @return  A list ready to extend to data_files.
+        """
+        data_files = []
+        buildDir = 'build'
+        if not os.path.exists(buildDir):
+            info('creating %s directory' % buildDir)
+            os.makedirs(buildDir)
+
+        str = {'build': buildDir, 'file': file, 'fileLower': file.lower()}
+        cmd = 'gzip -c --best %(file)s > %(build)s/%(fileLower)s.gz' % str
+        info('gzipping %s' % file)
+        if os.system(cmd) != 0:
+            raise SystemExit('Error while gzipping %(file)s' % str)
+        data_files.append((dest, ['%(build)s/%(fileLower)s.gz' % str]))
+
+        return data_files
+
+setup(name = "bluetracker",
+      version = "0.0.1",
+      description = "Bluetooth device scanner daemon.",
+      author = "Roel Huybrechts",
+      author_email = "mail@rulus.be",
+      license = "GPLv3",
+      packages = ["bluetracker"],
+      data_files = [("/etc/init.d", ['bluetrackd'])],
+      cmdclass = {'install_data': InstallData})
