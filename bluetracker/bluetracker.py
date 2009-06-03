737ca875c8bf60e2b6a169661759f4dc32031953
Roel Huybrechts <mail@rulus.be>
2009-06-03 14:31:18 +0200

    [gyrid @ mail@rulus.be-20090603123118-wg67nkdrn2gaaf0l] Switched from HAL to BlueZ.

--- bluetracker/bluetracker.py
+++ bluetracker/bluetracker.py
@@ -66,9 +66,8 @@ class Main(daemon.Daemon):
         self.main_loop = gobject.MainLoop()
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         self._dbus_systembus = dbus.SystemBus()
-        hal_obj = self._dbus_systembus.get_object(
-            'org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
-        self._dbus_hal = dbus.Interface(hal_obj, 'org.freedesktop.Hal.Manager')
+        bluez_obj = self._dbus_systembus.get_object('org.bluez', '/')
+        self._dbus_bluez = dbus.Interface(bluez_obj, 'org.bluez.Manager')
 
     def _handle_exception(self, etype, evalue, etraceback):
         """
@@ -92,8 +91,8 @@ class Main(daemon.Daemon):
     def run(self, restart=False):
         """
         Called after the daemon gets the (re)start command.
-        Connect the DeviceAdded signal (DBus/HAL) to its handler and start
-        the Bluetooth discoverer.
+        Connect the AdapterAdded signal (DBus/BlueZ) to its handler and
+        start the Bluetooth discoverer.
 
         @param  restart  If this call is part of a restart operation.
         """
@@ -105,11 +104,17 @@ class Main(daemon.Daemon):
             self.debug("Restarted")
 
         self._dbus_systembus.add_signal_receiver(self._bluetooth_device_added,
-            "DeviceAdded",
-            "org.freedesktop.Hal.Manager",
-            "org.freedesktop.Hal",
-            "/org/freedesktop/Hal/Manager")
-        self.debug("Connected to HAL DeviceAdded D-Bus signal")
+            bus_name = "org.bluez",
+            signal_name = "AdapterAdded")            
+        self.debug("Connected to BlueZ AdapterAdded D-Bus signal")
+        
+        for adapter in self._dbus_bluez.ListAdapters():
+            adap_obj = self._dbus_systembus.get_object('org.bluez', adapter)
+            adap_iface = dbus.Interface(adap_obj, 'org.bluez.Adapter')
+            adap_iface.SetProperty('Discoverable', False)
+            adap_iface.SetProperty('Pairable', False)
+            self.debug("Found Bluetooth adapter with address %s" %
+                adap_iface.GetProperties()['Address'])
 
         self._start_discover()
         self.main_loop.run()
@@ -125,9 +130,9 @@ class Main(daemon.Daemon):
         try:
             self.discoverer = discoverer.Discoverer(self, self.logger)
         except bluetooth.BluetoothError:
-            #No Bluetooth receiver found, return to end the function.
-            #We will automatically start again after a Bluetooth device
-            #has been plugged in thanks to HAL signal receiver.
+            #No Bluetooth adapter found, return to end the function.
+            #We will automatically start again after a Bluetooth adapter
+            #has been plugged in thanks to BlueZ signal receiver.
             return
 
         self.debug("Started scanning")
@@ -137,35 +142,30 @@ class Main(daemon.Daemon):
                 self.discoverer.process_event()
             except bluetooth._bluetooth.error, e:
                 if e[0] == 32:
-                    #The Bluetooth receiver has been plugged out, end the loop.
-                    #We will automatically start again after a Bluetooth device
-                    #has been plugged in thanks to HAL signal receiver.
-                    self.debug("Bluetooth receiver lost")
-                    self.logger.write_info("E: Bluetooth receiver lost")
+                    #The Bluetooth adapter has been plugged out, end the loop.
+                    #We will automatically start again after a Bluetooth adapter
+                    #has been plugged in thanks to BlueZ signal receiver.
+                    self.debug("Bluetooth adapter lost")
+                    self.logger.write_info("E: Bluetooth adapter lost")
                     self.logger.stop()
                     self.discoverer.done = True
         self.debug("Stopped scanning")
         del(self.discoverer)
 
-    def _bluetooth_device_added(self, sender=None):
+    def _bluetooth_device_added(self, path=None):
         """
-        Callback function for HAL signal. This method is automatically called
-        after a new hardware device has been plugged in. We check if is has
-        Bluetooth capabilities, and start scanning if it does.
+        Callback function for BlueZ signal. This method is automatically called
+        after a new Bluetooth device has been plugged in. Start scanning.
 
-        @param  sender   The device that has been plugged in.
+        @param  path   The device that has been plugged in.
         """
-        device_obj = self._dbus_systembus.get_object("org.freedesktop.Hal", sender)
-        device = dbus.Interface(device_obj, "org.freedesktop.Hal.Device")
-        try:
-            if ('bluetooth_hci' in device.GetProperty('info.capabilities')) and \
-               (not 'discoverer' in self.__dict__):
-                self.logger.write_info("I: Bluetooth receiver found")
-                self.debug("Bluetooth receiver found")
-                self._start_discover()
-        except dbus.DBusException:
-            #Raised when no such property exists.
-            pass
+        device_obj = self._dbus_systembus.get_object("org.bluez", path)
+        device = dbus.Interface(device_obj, "org.bluez.Adapter")
+
+        if not 'discoverer' in self.__dict__:
+            self.logger.write_info("I: Bluetooth adapter found")
+            self.debug("Bluetooth adapter found")
+            self._start_discover()
 
     def stop(self, debug=False, restart=False):
         """
