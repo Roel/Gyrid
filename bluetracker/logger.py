7fc03a21528055dab1067b36c42f22884061426c
Roel Huybrechts <mail@rulus.be>
2009-06-23 15:13:30 +0200

    [gyrid @ mail@rulus.be-20090623131330-udko3d2sbdhgen03] Use locking and temporary device pool to fix interfering threads.

--- bluetracker/logger.py
+++ bluetracker/logger.py
@@ -53,8 +53,9 @@ class Logger(object):
         self.started = False
         
         self.pool = {}
-        self.to_add = {}
+        self.temp_pool = {}
         self.poolchecker = PoolChecker(self.main, self)
+        self.lock = threading.Lock()
         
     def write(self, timestamp, mac_address, device_class, moving):
         """
@@ -87,12 +88,25 @@ class Logger(object):
         @param  mac_address    Hardware address of the Bluetooth device.
         @param  device_class   Device class of the Bluetooth device.
         """
-        self.switch_led(3)
-        
-        if mac_address not in self.pool and mac_address not in self.to_add:
-            self.to_add[mac_address] = [timestamp, device_class]
-        elif mac_address in self.pool:
-            self.pool[mac_address] = [timestamp, device_class]
+        if not self.lock.acquire(False):
+            #Failed to lock
+            if mac_address not in self.pool:
+                self.write(timestamp, mac_address, device_class, 'in')
+            self.temp_pool[mac_address] = [timestamp, device_class]
+        else:
+            try:
+                if len(self.temp_pool) > 0:
+                    self.pool.update(self.temp_pool)
+                    self.main.debug("%i devices in temporary pool, merging" % len(self.temp_pool))
+                    self.temp_pool.clear()
+                self.switch_led(3)
+                
+                if mac_address not in self.pool:
+                    self.write(timestamp, mac_address, device_class, 'in')
+                
+                self.pool[mac_address] = [timestamp, device_class]
+            finally:
+                self.lock.release()
                     
     def start(self):
         """
@@ -154,39 +168,39 @@ class PoolChecker(threading.Thread):
         """
         previous = 0
         while self._running:
-            self.logger.switch_led(2)
+            self.logger.lock.acquire()
+            try:
+                self.logger.switch_led(2)
 
-            tijd = int(time.time())
+                tijd = int(time.time())
 
-            to_delete = []
-            for device in self.logger.pool:
-                if tijd - self.logger.pool[device][0] > self.buffer:
-                    self.logger.write(self.logger.pool[device][0],
-                                      device,
-                                      self.logger.pool[device][1],
-                                      'out')
-                    to_delete.append(device)
-            
-            # Delete
-            for device in to_delete:
-                del(self.logger.pool[device])
-
-            # Add
-            self.logger.pool.update(self.logger.to_add)
-            for device in self.logger.to_add:
-                self.logger.write(self.logger.to_add[device][0],
-                                  device,
-                                  self.logger.to_add[device][1],
-                                  'in')
+                to_delete = []
+                for device in self.logger.pool:
+                    if tijd - self.logger.pool[device][0] > self.buffer:
+                        self.logger.write(self.logger.pool[device][0],
+                                          device,
+                                          self.logger.pool[device][1],
+                                          'out')
+                        to_delete.append(device)
                 
-            d = {'current': len(self.logger.pool),
-                 'new': len(self.logger.to_add),
-                 'gone': len(to_delete)}
-                 
-            self.logger.to_add.clear()
+                new = len(self.logger.pool) - previous
+                # Delete
+                for device in to_delete:
+                    del(self.logger.pool[device])
+                    
+                current = len(self.logger.pool)
 
-            self.main.debug("Device pool checked: %(current)i devices; " % d + \
+                d = {'current': current,
+                     'new': new if new > 0 else 0,
+                     'gone': len(to_delete)}
+                previous = current
+                
+                self.main.debug("Device pool checked: %(current)i devices; " % d + \
                 "%(new)i new; %(gone)i disappeared" % d)
+                
+            finally:
+                self.logger.lock.release()
+                
             time.sleep(self.buffer)
             
     def stop(self):
