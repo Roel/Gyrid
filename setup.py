0ea6fbee908bb3b248fcc2c21af4e20feef1e3fe
Roel Huybrechts <mail@rulus.be>
2009-06-24 20:09:23 +0200

    [gyrid @ mail@rulus.be-20090624180923-nosgtv21jae8mq9j] Prepare for 0.0.11 release.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ class InstallData(install_data):
         return data_files
 
 setup(name = "bluetracker",
-      version = "0.0.10",
+      version = "0.0.11",
       description = "Bluetooth device scanner daemon.",
       author = "Roel Huybrechts",
       author_email = "roel.huybrechts@ugent.be",
