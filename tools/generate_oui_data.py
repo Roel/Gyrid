3b719d4b810a355ae94480a04ea0c679c3083147
Roel Huybrechts <mail@rulus.be>
2009-10-14 21:36:00 +0200

    Added script to generate a compact tabseparated file linking MAC-addresses to vendors based on online IEEE data.

---
+++ tools/generate_oui_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#
+# This file belongs to Bluetracker.
+#
+# Bluetracker is a Bluetooth device scanner daemon.
+# Copyright (C) 2009  Roel Huybrechts
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Script to write a tabseparated 'oui_data.txt' file based on IEEE data.
+"""
+
+if __name__ == '__main__':
+    import urllib
+
+    input = urllib.urlopen("http://standards.ieee.org/regauth/oui/oui.txt", 'r')
+    output = open('oui_data.txt', 'w')
+
+    output.write("#Original data downloaded from http://standards.ieee.org/regauth/oui/oui.txt")
+    output.write("\n#\n#MAC-address\tVendor\n")
+    for line in input:
+        if '(hex)' in line:
+            mac = line[:8].replace('-', ':')
+            vendor = line.split('\t\t')[-1].strip(' \r\n')
+            output.write('\t'.join([mac, vendor]) + '\n')
+
+    input.close()
+    output.close()
