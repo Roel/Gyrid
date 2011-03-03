b7c06f439d02f11e244e706032dcb2543294fac7
Roel Huybrechts <mail@rulus.be>
2011-03-03 20:45:11 +0100

    Trivial: update copyright timestamps.

--- network_middleware.py
+++ network_middleware.py
@@ -4,7 +4,7 @@
 # This file belongs to Gyrid.
 #
 # Gyrid is a Bluetooth device scanner daemon.
-# Copyright (C) 2010  Roel Huybrechts
+# Copyright (C) 2010-2011  Roel Huybrechts
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
