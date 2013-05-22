3897375549ef41e88e9a0caa286b86f65d7e4227
Roel Huybrechts <roel.huybrechts@ugent.be>
2013-05-22 14:03:15 +0200

    Merge stash.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ wigy = Extension("wigy",
             libraries = ["iw"])
 
 setup(name = "gyrid",
-      version = "0.8.3",
+      version = "0.8.4",
       description = "Mobile device scanner.",
       author = "Roel Huybrechts",
       author_email = "roel.huybrechts@ugent.be",
