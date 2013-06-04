2b7abb6b56f101f1ddf17303c5ba18f66bab203a
Roel Huybrechts <roel.huybrechts@ugent.be>
2013-06-04 09:52:51 +0200

    Prepare for 0.8.6 release.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ wigy = Extension("wigy",
             libraries = ["iw"])
 
 setup(name = "gyrid",
-      version = "0.8.5",
+      version = "0.8.6",
       description = "Mobile device scanner.",
       author = "Roel Huybrechts",
       author_email = "roel.huybrechts@ugent.be",
